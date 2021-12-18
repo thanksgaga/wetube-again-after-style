f65888f97c4129583fc1d2a4fe77edd82d665fd0
thanks.gaga <coach.gayoung@gmail.com>
2021-12-19 01:03:57 +0800

    #16.8 cooment Ids in real time update

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,16 +1,20 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const addComment = (text) => {
+const addComment = (text, id) => {
 	const videoComments = document.querySelector(".video__comments ul");
 	const newComment = document.createElement("li");
 	newComment.className = "video__comment";
+	newComment.dataset.id = id;
 	const icon = document.createElement("i");
 	icon.className = "fas fa-comment";
 	const span = document.createElement("span");
 	span.innerText = ` ${text}`;
+	const span2 = document.createElement("span");
+	span2.innerText = "💋";
 	newComment.appendChild(icon);
 	newComment.appendChild(span);
+	newComment.appendChild(span2);
 	videoComments.prepend(newComment);
 };
 const handleSubmit = async (event) => {
@@ -21,18 +25,18 @@ const handleSubmit = async (event) => {
 	if (text === "") {
 		return;
 	}
-	const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+	const response = await fetch(`/api/videos/${videoId}/comment`, {
 		method: "post",
 		headers: {
 			"Content-Type": "application/json",
 		},
 		body: JSON.stringify({ text }),
 	});
-	textarea.value = "";
 	//window.location.reload();
-	if (status === 201) {
-		addComment(text);
-		console.log("create fake comment");
+	if (response.status === 201) {
+		textarea.value = "";
+		const { newCommentId } = await response.json();
+		addComment(text, newCommentId);
 	}
 };
 
