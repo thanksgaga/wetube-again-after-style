bf24630edd5522691eedfc48ee7f49644954967e
thanks.gaga <coach.gayoung@gmail.com>
2021-12-19 14:44:15 +0800

    change URL on console for logging

--- src/init.js
+++ src/init.js
@@ -9,6 +9,8 @@ import app from "./server";
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-	console.log(`✅ Server listenting on http://localhost:${PORT} 🚀`);
+	console.log(
+		`✅ Server listenting on https://wetube-reloaded-gaga.herokuapp.com:${PORT} 🚀`,
+	);
 
 app.listen(PORT, handleListening);
