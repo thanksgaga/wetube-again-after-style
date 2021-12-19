a2f0160b2a33f674739326a3b966105f36d3fa9b
thanks.gaga <coach.gayoung@gmail.com>
2021-12-19 10:15:00 +0800

    Revert "client code clean up"
    This reverts commit 03ff4599820cfc77a9c31912af60b2a6129cbe82.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -18,93 +18,94 @@ let volumeValue = 0.5;
 video.volume = volumeValue;
 
 const handlePlayClick = (e) => {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
-  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
+	if (video.paused) {
+		video.play();
+	} else {
+		video.pause();
+	}
+	playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 
 const handleMuteClick = (e) => {
-  if (video.muted) {
-    video.muted = false;
-  } else {
-    video.muted = true;
-  }
-  muteBtnIcon.classList = video.muted
-    ? "fas fa-volume-mute"
-    : "fas fa-volume-up";
-  volumeRange.value = video.muted ? 0 : volumeValue;
+	if (video.muted) {
+		video.muted = false;
+	} else {
+		video.muted = true;
+	}
+	muteBtnIcon.classList = video.muted
+		? "fas fa-volume-mute"
+		: "fas fa-volume-up";
+	volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
 const handleVolumeChange = (event) => {
-  const {
-    target: { value },
-  } = event;
-  if (video.muted) {
-    video.muted = false;
-    muteBtn.innerText = "Mute";
-  }
-  volumeValue = value;
-  video.volume = value;
+	const {
+		target: { value },
+	} = event;
+	if (video.muted) {
+		video.muted = false;
+		muteBtn.innerText = "Mute";
+	}
+	volumeValue = value;
+	video.volume = value;
 };
 
 const formatTime = (seconds) =>
-  new Date(seconds * 1000).toISOString().substr(14, 5);
+	new Date(seconds * 1000).toISOString().substr(14, 5);
 
 const handleLoadedMetadata = () => {
-  totalTime.innerText = formatTime(Math.floor(video.duration));
-  timeline.max = Math.floor(video.duration);
+	totalTime.innerText = formatTime(Math.floor(video.duration));
+	timeline.max = Math.floor(video.duration);
 };
 
 const handleTimeUpdate = () => {
-  currenTime.innerText = formatTime(Math.floor(video.currentTime));
-  timeline.value = Math.floor(video.currentTime);
+	currenTime.innerText = formatTime(Math.floor(video.currentTime));
+	timeline.value = Math.floor(video.currentTime);
 };
 
 const handleTimelineChange = (event) => {
-  const {
-    target: { value },
-  } = event;
-  video.currentTime = value;
+	const {
+		target: { value },
+	} = event;
+	video.currentTime = value;
 };
 
 const handleFullscreen = () => {
-  const fullscreen = document.fullscreenElement;
-  if (fullscreen) {
-    document.exitFullscreen();
-    fullScreenIcon.classList = "fas fa-expand";
-  } else {
-    videoContainer.requestFullscreen();
-    fullScreenIcon.classList = "fas fa-compress";
-  }
+	const fullscreen = document.fullscreenElement;
+	if (fullscreen) {
+		document.exitFullscreen();
+		fullScreenIcon.classList = "fas fa-expand";
+	} else {
+		videoContainer.requestFullscreen();
+		fullScreenIcon.classList = "fas fa-compress";
+	}
 };
 
 const hideControls = () => videoControls.classList.remove("showing");
 
 const handleMouseMove = () => {
-  if (controlsTimeout) {
-    clearTimeout(controlsTimeout);
-    controlsTimeout = null;
-  }
-  if (controlsMovementTimeout) {
-    clearTimeout(controlsMovementTimeout);
-    controlsMovementTimeout = null;
-  }
-  videoControls.classList.add("showing");
-  controlsMovementTimeout = setTimeout(hideControls, 3000);
+	if (controlsTimeout) {
+		clearTimeout(controlsTimeout);
+		controlsTimeout = null;
+	}
+	if (controlsMovementTimeout) {
+		clearTimeout(controlsMovementTimeout);
+		controlsMovementTimeout = null;
+	}
+	videoControls.classList.add("showing");
+	controlsMovementTimeout = setTimeout(hideControls, 3000);
 };
 
 const handleMouseLeave = () => {
-  controlsTimeout = setTimeout(hideControls, 3000);
+	controlsTimeout = setTimeout(hideControls, 3000);
 };
 
 const handleEnded = () => {
-  const { id } = videoContainer.dataset;
-  fetch(`/api/videos/${id}/view`, {
-    method: "POST",
-  });
+	const { id } = videoContainer.dataset;
+	console.log("handleEnded" + id);
+	fetch(`/api/videos/${id}/view`, {
+		method: "POST",
+	});
 };
 
 playBtn.addEventListener("click", handlePlayClick);
