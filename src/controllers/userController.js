65a789195170bfab9a5e7569e5a5417860551c18
thanks.gaga <coach.gayoung@gmail.com>
2021-12-19 10:14:54 +0800

    Revert "views and controller cleanup"
    This reverts commit 48bb7e964fd0b9d5946d5aa76d923df941d65af8.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -135,7 +135,6 @@ export const finishGithubLogin = async (req, res) => {
 
 export const logout = (req, res) => {
 	req.session.destroy();
-	req.flash("info", "Bye Bye");
 	return res.redirect("/");
 };
 export const getEdit = (req, res) => {
@@ -166,7 +165,6 @@ export const postEdit = async (req, res) => {
 
 export const getChangePassword = (req, res) => {
 	if (req.session.user.socialOnly === true) {
-		req.flash("error", "Can't change password.");
 		return res.redirect("/");
 	}
 	return res.render("users/change-password", { pageTitle: "Change Password" });
@@ -194,7 +192,6 @@ export const postChangePassword = async (req, res) => {
 	}
 	user.password = newPassword;
 	await user.save();
-	req.flash("info", "Password updated");
 	return res.redirect("/users/logout");
 };
 
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,6 @@
 import Video from "../models/Video";
-import Comment from "../models/Comment";
 import User from "../models/User";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
 	const videos = await Video.find({})
@@ -12,10 +12,10 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
 	const { id } = req.params;
 	const video = await Video.findById(id).populate("owner").populate("comments");
-	console.log(video);
 	if (!video) {
 		return res.render("404", { pageTitle: "Video not found." });
 	}
+	console.log(video);
 	return res.render("watch", { pageTitle: video.title, video });
 };
 
@@ -29,7 +29,6 @@ export const getEdit = async (req, res) => {
 		return res.status(404).render("404", { pageTitle: "Video not found." });
 	}
 	if (String(video.owner) !== String(_id)) {
-		req.flash("error", "Not authorized");
 		return res.status(403).redirect("/");
 	}
 	return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
@@ -46,7 +45,6 @@ export const postEdit = async (req, res) => {
 		return res.status(404).render("404", { pageTitle: "Video not found." });
 	}
 	if (String(video.owner) !== String(_id)) {
-		req.flash("error", "You are not the the owner of the video.");
 		return res.status(403).redirect("/");
 	}
 	await Video.findByIdAndUpdate(id, {
@@ -54,7 +52,6 @@ export const postEdit = async (req, res) => {
 		description,
 		hashtags: Video.formatHashtags(hashtags),
 	});
-	req.flash("success", "Changes saved.");
 	return res.redirect(`/videos/${id}`);
 };
 
@@ -66,14 +63,13 @@ export const postUpload = async (req, res) => {
 	const {
 		user: { _id },
 	} = req.session;
-	const { video, thumb } = req.files;
+	const { path: fileUrl } = req.file;
 	const { title, description, hashtags } = req.body;
 	try {
 		const newVideo = await Video.create({
 			title,
 			description,
-			fileUrl: video[0].path,
-			thumbUrl: thumb[0].path,
+			fileUrl,
 			owner: _id,
 			hashtags: Video.formatHashtags(hashtags),
 		});
